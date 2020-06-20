ac5fae6073f6632d41242a6af4f2626359fbb929
Aravind Reddy P <ponugotiaravind@gmail.com>
2020-06-20 11:50:50 -0400

    second commit

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 // strike through functionality
-$("ul").on("click","li", function(){
-    $(this).toggleClass("completed");
 
+$(document).ready(function () {
+  $('ul').on('click', 'li', function () {
+    $(this).toggleClass('completed');
+  });
 });
 
 // deleting
-$("ul").on("click","span" ,function(event) {
-    $(this).parent().fadeOut(200,function(){
+$(document).ready(function () {
+  $('ul').on('click', 'span', function (event) {
+    $(this)
+      .parent()
+      .fadeOut(100, function () {
         $(this).remove();
-    });
+      });
     event.stopPropagation();
-
+  });
 });
 
 // adding
 
-$("input").keypress(function(event) {
-    if(event.which === 13) {
-        var task = $("input").val();
-        $("ul").append("<li><span class='del'><i class='fas fa-trash'></i> </span>" + ' '+ task + " </li>");
-        $("input").val(" ");
-    }   
+$('input').keypress(function (event) {
+  var task = $('input').val();
+  if ((task == null || task == ' ') && event.which === 13) {
+    alert('Please enter a Task!!');
+  } else {
+    if (event.which === 13) {
+      var task = $('input').val();
+      $('ul').append(
+        "<li><span class='del'><i class='fas fa-trash'></i> </span>" +
+          ' ' +
+          task +
+          ' </li>'
+      );
+      $('input').val(' ');
+    }
+  }
 });
 
 //toggle input
-$("#add").click(function() {
-    $("input").fadeToggle();
+$('#add').click(function () {
+  $('input').fadeToggle();
 });
